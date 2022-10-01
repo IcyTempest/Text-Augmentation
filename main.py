9aeb54d1478997e0fe3faac003820e04edaff1e3
Vibol Rithiya <vibolrithiya@gmail.com>
2022-10-01 10:42:48 +0700

    Edit README

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from models import MyWords, Intent
+from models import MyWords, Intent, convert
 from file import fileio
 
 
-def sentences(filename, word1, word2, word3, word4):
+def sentences(filename, word1, word2, word3, word4, segment: bool = False):
     tmp = "\n word1:" + str(word1) + "\n " + "word2:" + str(word2) + "\n " + "word3:" + str(
         word3) + "\n " + "word4:" + str(word4) + "\n " + "_______________________________________________"
     fileio(filename, tmp)
@@ -10,33 +10,49 @@ def sentences(filename, word1, word2, word3, word4):
         for j in range(len(word2)):
             for k in range(len(word3)):
                 for m in range(len(word4)):
-                    x = "តើ/O " + word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1]
-                    print(x)
-                    fileio(filename, x)
+                    if segment:
+                        x = "តើ/O " + word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1]
+                        print(x)
+                        fileio(filename, x)
+                    else:
+                        x = "តើ" + word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1]
+                        print(x)
+                        fileio(filename, x)
 
 
 def intent():
-    filename = 'rose'
-    # If dak word normally, remove intent
-    words1: MyWords = MyWords(intentR=Intent.I,
-                              words=[" Manuth ", "ម៉ាន៉ុត", " Rithiya ", "រិទ្ធិយ៉ា", " Chhunheang ", "ឈុនហ៊ាង",
-                                     " Kimhong ", "គីមហុង", " Nary ",
-                                     "ណារី", " Nao ", "ណោ"])
+    filename = 'newPage'
+    # If dak word normally, modify segment to false
+    # If wants to segment word, add space to each word,
+    # for Intent.I, adding space for entity is optional
+    segment: bool = False
 
-    # Add space at the end of the sentence too, im too lazy to account for this XD
-    words2: MyWords = MyWords(intentR=Intent.O,
-                              words=["មាន ដឹង សាលា មាន ", "ដឹង ថា សាលា មាន "])
+    words1 = convert(
+        intent=Intent.I,
+        segment=segment,
+        words=[" Manuth ", "ម៉ាន៉ុត", " Rithiya ", "រិទ្ធិយ៉ា", " Chhunheang ", "ឈុនហ៊ាង",
+               " Kimhong ", "គីមហុង", " Nary ",
+               "ណារី", " Nao ", "ណោ"])
 
-    words3: MyWords = MyWords(intentR=Intent.I,
-                              words=[' Facebook page ', "ហ្វេសបុកភេច", " social media page ", "សូសលមីឌិរភេច", " page ",
-                                     "ភេច",
-                                     " instagram page ", "អុីនស្តាក្រាម ភេច", " Twitter ", "ថ្វីតធឺរ", " TikTok ",
-                                     "តិកតុក"])
+    words2 = convert(
+        intent=Intent.O,
+        segment=segment,
+        words=["មាន ដឹង សាលា មាន ", "ដឹង ថា សាលា មាន "])
 
-    words4: MyWords = MyWords(intentR=Intent.O,
-                              words=["អត់?", "ទេ?"])
+    words3 = convert(
+        intent=Intent.I,
+        segment=segment,
+        words=[' Facebook page ', "ហ្វេសបុកភេច", " social media page ", "សូសលមីឌិរភេច", " page ",
+               "ភេច",
+               " instagram page ", "អុីនស្តាក្រាម ភេច", " Twitter ", "ថ្វីតធឺរ", " TikTok ",
+               "តិកតុក"])
 
-    sentences(filename, word1=words1.words, word2=words2.words, word3=words3.words, word4=words4.words)
+    words4 = convert(
+        intent=Intent.O,
+        segment=segment,
+        words=["អត់?", "ទេ?"])
+
+    sentences(filename, word1=words1, word2=words2, word3=words3, word4=words4, segment=segment)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -9,31 +9,36 @@ class Intent(Enum):
     I = "/I-PER"
 
 
-def convert(intent: Intent, words: List[str]):
+def convert(words: List[str], intent: Intent = None, segment:bool = True):
     match intent:
         case Intent.I:
+            if segment:
+                # Long version
+                # rose = []
+                # rise = []
+                # for i in words:
+                #     rose = i.strip()
+                #     rose += " "
+                #     rise.append(rose)
+                #     print("|" + rose + "|")
+                #     for j in rise:
+                #         ree = j.replace(" ", "/B-PER ")
+                #         roo = re.sub(r"/B-PER $", "/I-PER ", ree)
+                #         list.append(roo)
 
-            # Long version
-            # rose = []
-            # rise = []
-            # for i in words:
-            #     rose = i.strip()
-            #     rose += " "
-            #     rise.append(rose)
-            #     print("|" + rose + "|")
-            #     for j in rise:
-            #         ree = j.replace(" ", "/B-PER ")
-            #         roo = re.sub(r"/B-PER $", "/I-PER ", ree)
-            #         list.append(roo)
 
-
-            # Short Version
-            ree = [x.strip()+" " for x in words]
-            row = [re.sub(r"/B-PER $", "/I-PER ", x.replace(" ", "/B-PER ")) for x in ree]
-            return row
+                # Short Version
+                ree = [x.strip()+" " for x in words]
+                row = [re.sub(r"/B-PER $", "/I-PER ", x.replace(" ", "/B-PER ")) for x in ree]
+                return row
+            return words
 
         case Intent.O:
-            return [x.replace(" ", intent.value + " ").replace("?", "?" + intent.value) for x in words]
+
+            if segment:
+                return [x.replace(" ", intent.value + " ").replace("?", "?" + intent.value) for x in words]
+            return [x.replace(" ", "") for x in words]
+
         case _:
             return words
 
