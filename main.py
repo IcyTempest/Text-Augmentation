250781ae7600de94242f6a3486530bd4e2d484ea
Vibol Rithiya <vibolrithiya@gmail.com>
2022-10-02 15:24:02 +0700

    Fixed bug, not showing B-Entity during the first word.

--- main.py
+++ main.py
@@ -1,69 +1,109 @@
+from typing import List
+
 from models import Intent, convert
 from file import fileio
 
 
-def sentences(filename, word1, word2, word3, word4, segment: bool = False):
+def sentences(filename, word1, word2, word3, word4, word5, segment: bool = False):
     tmp = "\n word1:" + str(word1) + "\n " + "word2:" + str(word2) + "\n " + "word3:" + str(
-        word3) + "\n " + "word4:" + str(word4) + "\n " + "_______________________________________________"
+        word3) + "\n " + "word4:" + str(word4) + "\n " + "word5:" + str(
+        word5) + "\n " + "_______________________________________________"
     fileio(filename, tmp)
+
     for i in range(len(word1)):
         for j in range(len(word2)):
             for k in range(len(word3)):
                 for m in range(len(word4)):
-                    if segment:
-                        x = "តើ/O " + word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1]
-                        print(x)
-                        fileio(filename, x)
-                    else:
-                        x = "តើ" + word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1]
-                        print(x)
-                        fileio(filename, x)
+                    for r in range(len(word5)):
+                        if segment:
+                            x = "តើ/O " + word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1] + word5[r - 1]
+                            print(x)
+                            fileio(filename, x)
+                        else:
+                            x = "តើ" + word1[i - 1] + word2[j - 1] + word3[k - 1] + word4[m - 1] + word5[r - 1]
+                            print(x)
+                            fileio(filename, x)
 
 
-def SocialMediaExist():
-    filename = 'newPage'
-    # If dak word normally, modify segment to false
-    # If wants to segment word, add space to each word,
-    # for Intent.I, adding space for entity is optional
+# def SocialMediaExist():
+#     filename = 'newPage'
+#     # If dak word normally, modify segment to false
+#     # If wants to segment word, add space to each word,
+#     # for Intent.I, adding space for entity is optional
+#     segment: bool = True
+#
+#     words1 = convert(
+#         intent=Intent.Person,
+#         segment=segment,
+#         words=[" Manuth ", "ម៉ាន៉ុត", " Rithiya ", "រិទ្ធិយ៉ា", " Chhunheang ", "ឈុនហ៊ាង",
+#                " Kimhong ", "គីមហុង", " Nary ",
+#                "ណារី", " Nao ", "ណោ"])
+#
+#     words2 = convert(
+#         intent=Intent.O,
+#         segment=segment,
+#         words=["មាន ដឹង សាលា មាន ", "ដឹង ថា សាលា មាន "])
+#
+#     words3 = convert(
+#         intent=Intent.Organization,
+#         segment=segment,
+#         words=[' Facebook page ', "ហ្វេសបុកភេច", " social media page ", "សូសលមីឌិរភេច", " page ",
+#                "ភេច",
+#                " instagram page ", "អុីនស្តាក្រាម ភេច", " Twitter ", "ថ្វីតធឺរ", " TikTok ",
+#                "តិកតុក"])
+#
+#     words4 = convert(
+#         intent=Intent.O,
+#         segment=segment,
+#         words=["អត់?", "ទេ?"])
+#
+#     sentences(filename, word1=words1, word2=words2, word3=words3, word4=words4, segment=segment)
+#
+#
+# def announceScholarship():
+#     filename = 'announceScholarship'
+#     # If dak word normally, modify segment to false
+#     # If wants to segment word, add space to each word,
+#     # for Intent.I, adding space for entity is optional
+#     segment: bool = False
+#
+#     words1 = convert(
+#         intent=Intent.I,
+#         segment=segment,
+#         words=[" Darot ", "ដារូ៉ត", " Jojo ", "ចូចូ", " Lita ", "លីតា",
+#                " Duoung ", "ដួង", " Linda ",
+#                "លីនដា", " Sokleap ", "សុខលាភ"])
+#
+#     words2 = convert(
+#         intent=Intent.O,
+#         segment=segment,
+#         words=["ដឹង ថា សាលា បាន ប្រកាស ", "មាន ដឹង សាលា មាន ", ])
+#
+#     # words3 = convert(
+#     #     intent=Intent.I,
+#     #     segment=segment,
+#     #     words=[' Facebook page ', "ហ្វេសបុកភេច", " social media page ", "សូសលមីឌិរភេច", " page ",
+#     #            "ភេច",
+#     #            " instagram page ", "អុីនស្តាក្រាម ភេច", " Twitter ", "ថ្វីតធឺរ", " TikTok ",
+#     #            "តិកតុក"])
+#     words3 = convert(
+#         intent=Intent.I,
+#         segment=segment,
+#         words=["Scholarship Event", "អាហារូបករណ៍ event", "អាហារូបករណ៍ អុីវែន"])
+#
+#     words4 = convert(
+#         intent=Intent.O,
+#         segment=segment,
+#         words=["នៅ?", "ទេ?"])
+#
+#     sentences(filename, word1=words1, word2=words2, word3=words3, word4=words4, segment=segment)
+
+def findTotalStudent():
+    filename = 'FindTotalStudentsV2'
     segment: bool = True
 
     words1 = convert(
         intent=Intent.Person,
-        segment=segment,
-        words=[" Manuth ", "ម៉ាន៉ុត", " Rithiya ", "រិទ្ធិយ៉ា", " Chhunheang ", "ឈុនហ៊ាង",
-               " Kimhong ", "គីមហុង", " Nary ",
-               "ណារី", " Nao ", "ណោ"])
-
-    words2 = convert(
-        intent=Intent.O,
-        segment=segment,
-        words=["មាន ដឹង សាលា មាន ", "ដឹង ថា សាលា មាន "])
-
-    words3 = convert(
-        intent=Intent.Organization,
-        segment=segment,
-        words=[' Facebook page ', "ហ្វេសបុកភេច", " social media page ", "សូសលមីឌិរភេច", " page ",
-               "ភេច",
-               " instagram page ", "អុីនស្តាក្រាម ភេច", " Twitter ", "ថ្វីតធឺរ", " TikTok ",
-               "តិកតុក"])
-
-    words4 = convert(
-        intent=Intent.O,
-        segment=segment,
-        words=["អត់?", "ទេ?"])
-
-    sentences(filename, word1=words1, word2=words2, word3=words3, word4=words4, segment=segment)
-
-
-def announceScholarship():
-    filename = 'announceScholarship'
-    # If dak word normally, modify segment to false
-    # If wants to segment word, add space to each word,
-    # for Intent.I, adding space for entity is optional
-    segment: bool = False
-
-    words1 = convert(
-        intent=Intent.I,
         segment=segment,
         words=[" Darot ", "ដារូ៉ត", " Jojo ", "ចូចូ", " Lita ", "លីតា",
                " Duoung ", "ដួង", " Linda ",
@@ -72,27 +112,30 @@ def announceScholarship():
     words2 = convert(
         intent=Intent.O,
         segment=segment,
-        words=["ដឹង ថា សាលា បាន ប្រកាស ", "មាន ដឹង សាលា មាន ", ])
+        words=["ដឹង ថា  ", "មាន ដឹង សាលា មាន និស្សិត ", "ដឹង ថា សាលា មាន សិស្ស ", "មាន ដឹង សាលា មាន និស្សិត សរុប",
+               "មាន ដឹង សាលា មាន និស្សិត ", "មាន ដឹង ថា សាកលវិទ្យាល័យ មាន សិស្ស សរុប ",
+               "មាន ដឹង ថា សាកលវិទ្យាល័យ មាន សិស្ស ",
+               "មាន ដឹង ថា សាកលវិទ្យាល័យ មាន និស្សិត សរុប", "មាន ដឹង ថា សាកលវិទ្យាល័យ មាន និស្សិត "])
 
-    # words3 = convert(
-    #     intent=Intent.I,
-    #     segment=segment,
-    #     words=[' Facebook page ', "ហ្វេសបុកភេច", " social media page ", "សូសលមីឌិរភេច", " page ",
-    #            "ភេច",
-    #            " instagram page ", "អុីនស្តាក្រាម ភេច", " Twitter ", "ថ្វីតធឺរ", " TikTok ",
-    #            "តិកតុក"])
     words3 = convert(
-        intent=Intent.I,
+        intent=Intent.Building,
         segment=segment,
-        words=["Scholarship Event", "អាហារូបករណ៍ event", "អាហារូបករណ៍ អុីវែន"])
+        words=[" CADT ", "សុីអេឌុី", "សាលា CADT ", "សាកលវិទ្យាល័យ CADT ", "សាកលវិទ្យាល័យ សុីអេឌុី", "វិទ្យាល័យ CADT ",
+               "វិទ្យាល័យ សុីអេឌុី"])
 
     words4 = convert(
         intent=Intent.O,
         segment=segment,
-        words=["នៅ?", "ទេ?"])
+        words=["មាន សិស្ស សរុប "])
 
-    sentences(filename, word1=words1, word2=words2, word3=words3, word4=words4, segment=segment)
+    words5 = convert(
+        intent=Intent.O,
+        segment=segment,
+        words=["ប៉ុន្មាន?", "ប៉ុន្មាន អ្នក?", "ម៉ាន?", "ម៉ាន អត់?", "ម៉ាន ទេ?",
+               "ម៉ាន អ្នក?"])
+
+    sentences(filename, word1=words1, word2=words2, word3=words3, word4=words4, segment=segment, word5=words5)
 
 
 if __name__ == '__main__':
-    SocialMediaExist()
+    findTotalStudent()
--- models.py
+++ models.py
@@ -3,26 +3,27 @@ import re
 from enum import Enum, unique
 from typing import List
 
+
 @unique
 class Intent(Enum):
     O = "/O"
     Person = "PER"
     Room = "ROOM"
-    Building="BUILD"
-    Floor="FLOOR"
-    Ordinal="ORD"
-    Cardinal="CAR"
-    Date="DATE"
-    Time="TIME"
-    Money="MONEY"
-    Event="EVE"
-    Announcement="ANN"
-    CountryCity="GPE"
-    Location="LOC"
-    Organization="ORG"
+    Building = "BUILD"
+    Floor = "FLOOR"
+    Ordinal = "ORD"
+    Cardinal = "CAR"
+    Date = "DATE"
+    Time = "TIME"
+    Money = "MONEY"
+    Event = "EVE"
+    Announcement = "ANN"
+    CountryCity = "GPE"
+    Location = "LOC"
+    Organization = "ORG"
 
 
-def convert(words: List[str], intent: Intent = None, segment:bool = True):
+def convert(words: List[str], intent: Intent = None, segment: bool = True):
     match intent:
         case Intent.O:
 
@@ -32,7 +33,7 @@ def convert(words: List[str], intent: Intent = None, segment:bool = True):
 
         case _:
             if segment:
-                # Long version
+                # # Long version
                 # rose = []
                 # rise = []
                 # for i in words:
@@ -42,12 +43,17 @@ def convert(words: List[str], intent: Intent = None, segment:bool = True):
                 #     print("|" + rose + "|")
                 #     for j in rise:
                 #         ree = j.replace(" ", "/B-PER ")
-                #         roo = re.sub(r"/B-PER $", "/I-PER ", ree)
-                #         list.append(roo)
+                #         if ree.count(" ")>2:
+                #             roo = re.sub(r"/B-PER $", "/I-PER ", ree)
+                #             list.append(roo)
 
                 # Short Version
                 intent = intent.value
                 ree = [x.strip() + " " for x in words]
-                row = [re.sub(r"/B-{} $".format(intent), "/I-{} ".format(intent), x.replace(" ", "/B-{} ".format(intent))) for x in ree]
+                row = [re.sub(r"/B-{} $".format(intent), "/I-{} ".format(intent),
+                              x.replace(" ", "/B-{} ".format(intent))) if x.count(" ") >= 2 else x.replace(" ",
+                                                                                                          "/B-{} ".format(
+                                                                                                              intent))
+                       for x in ree]
                 return row
             return words
